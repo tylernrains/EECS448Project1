7bebbd5d14e34cd0b9d456e9aef4ebe1aa079f1f
Hunter Glass <h_glass@ku.edu>
2020-09-07 11:28:29 -0500

    added loop to test hits on the game board

--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@
 #include "Board.h"
 
 int main(){
-
-	cout << "    Battleship Game\n";
+	cout << "Battleship Game\n";
 	Board board1;
+	int numHits = 0;
 	board1.printBoard();
+	while(numHits < 3)
+	{
+		cout << "Enter coordinates to hit: ";
+		char row;
+		int col;
+		cin >> row >> col;
+		board1.updateBoard(row, col);
+		board1.printBoard();
+		numHits++;
+	}
 	return(0);
 }
