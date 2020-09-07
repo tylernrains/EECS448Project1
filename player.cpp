23fcac0368373d7c4f68665fa1524cef6e8581ca
Connor Sutton <connorsutton07@gmail.com>
2020-09-07 11:48:56 -0500

    Implemented PrintMyShips(), PrintEnemyShips(), ConvertCharCoord(), and PlaceShip() for Player class

--- player.cpp
+++ player.cpp
@@ -6,12 +6,63 @@ Player::Player() {}
 
 Player::~Player() {}
 
-void Player::printMyShips()
+void Player::PrintMyShips()
 {
     my_ships.printBoard();
 }
 
-void Player::printEnemyShips()
+void Player::PrintEnemyShips()
 {
     enemy_ships.printBoard();
+}
+
+int Player::ConvertCharCoord(char c)
+{
+    int a = toupper(c);
+    return (a - 65);
+}
+
+bool Player::PlaceShip(int size, int row, char c_col, bool horizontal)
+{
+    int col = ConvertCharCoord(c_col);
+    row -= 1;
+
+    if (row >= 0 && row < 9 && col >= 0 && col < 9)
+    {
+        if (horizontal)
+        {
+            if (9 - col >= size)
+            {
+                for (int j = col; j < col + size; j++)
+                {
+                    //if (my_board.checkSpot(row, j) == '-') return false;
+                }
+                for (int j = col; j < col + size; j++)
+                {
+                    //my_board.updateBoard(row, j, 'O')
+                }
+            }
+            else return false;
+        }
+        else
+        {
+            if (9 - row >= size)
+            {
+                for (int i = row; i < row + size; i++)
+                {
+                    //if (my_board.checkSpot(i, col) == '-') return false;
+                }
+                for (int i = row; i < row + size; i++)
+                {
+                    //my_board.updateBoard(i, col, 'O')
+                }
+            }
+            else return false;
+        }
+        
+    }
+    else return false;
+
+    return true;
+
 }
--- player.h
+++ player.h
@@ -9,11 +9,14 @@ class Player
 		Player();   	    // Constructor
 		~Player();  	    // Destructor;
 
-		void printMyShips();
-		void printEnemyShips();
-		void clearBoards();
+		bool PlaceShip(int size, int row, char c_col, bool horizontal); // will try to place a ship with upper left point at row, col; returns false if it cant 
 
-		bool checkHit();
+		void PrintMyShips(); // prints board with players ships
+		void PrintEnemyShips(); // prints board with hits/misses of enemy ships
+		void ClearBoards(); // returns both boards to original state
+
+		int ConvertCharCoord(char c); //Converts column letter to corresponding int index
+		bool CheckHit(); // returns true/false if a ship has been hit
 
 	private:
 		Board my_ships;
