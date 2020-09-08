10049a15b37ea4b7546c156c7a8794ceb8121ad5
Shujing Wu <s777w427@ku.edu>
2020-09-08 12:37:35 -0500

    add to fork

---
+++ Executive.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifdef EXECUTIVE_H
+#define EXECUTIVE_H
+#include "Board.h"
+#include "Player.h"
+
+#include <iostream>
+using namespace std;
+
+class Executive
+{
+public:
+
+
+	Executive(){};
+
+	void run();
+	~Executive(){};
+
+	
+};
+
+
+#endif // EXECUTIVE_H
