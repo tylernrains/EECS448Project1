180132cfc923c2871a85ffc1a99f512c10d04eb0
Octavio Sosa <octavio@ku.edu>
2020-09-08 22:39:29 -0500

    define generic board elements

--- display.h
+++ display.h
@@ -7,14 +7,18 @@ class Display
 {
 	private:
 	std::string m_borderSpace;
-	std::string m_playerBanner;
+	std::string m_playeriBanner;
+	std::string m_enemyBanner;
 	std::string m_colLabel;
+	std::string m_borderLineTop;
 	std::string m_rowiLabel;
 	std::string m_gridLine;
-	std::string m_borderLine;
+	std::string m_borderLineBottom;
 	std::string m_friendlyBanner;
 
 	public:
 	Display();
+	void genericBoard() const;
+	void genericFrame() const;
 };
 #endif
