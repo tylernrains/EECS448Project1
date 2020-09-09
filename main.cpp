0e78a18b47fe7eef7ccb857c5f7454e11199b80a
Octavio Sosa <octavio@ku.edu>
2020-09-09 09:51:34 -0500

    demos visual match elements

--- main.cpp
+++ main.cpp
@@ -2,11 +2,27 @@
 #include <string>
 #include "board.h"
 #include "player.h"
+#include "display.h"
 
 int main(){
+	/*
 	cout << "Battleship Game\n";
 	Board board1;
 	int numHits = 0;
+	*/
+
+	Display display;
+	//display.genericFrame();
+	char** enemyBoard = display.test_enemyBoard();
+	char** friendlyBoard = display.test_friendlyBoard();
+
+	//display.enemyBoard(enemyBoard, 1, 3);
+	//display.friendlyBoard(friendlyBoard);
+	display.matchFrame(2, 3, enemyBoard, friendlyBoard);
+	display.hit();
+	display.miss();
+
+	/*
 	board1.printBoard();
 	while(numHits < 3)
 	{
@@ -18,5 +34,6 @@ int main(){
 		board1.printBoard();
 		numHits++;
 	}
+	*/
 	return(0);
 }
