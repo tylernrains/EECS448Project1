6879aaa228a1be901f3dd7542493050e2b1641f1
Octavio Sosa <octavio@ku.edu>
2020-09-08 21:37:44 -0500

    initializes board elements

--- display.cpp
+++ display.cpp
@@ -3,5 +3,12 @@
 
 Display::Display()
 {
+	m_borderSpace = "";
+	m_playerBanner = "";
+	m_colLabel = "";
+	m_rowiLabel = "";
+	m_gridLine = "";
+	m_borderLine = "";
+	m_friendlyBanner = "";
 }
 
--- display.h
+++ display.h
@@ -7,6 +7,12 @@ class Display
 {
 	private:
 	std::string m_borderSpace;
+	std::string m_playerBanner;
+	std::string m_colLabel;
+	std::string m_rowiLabel;
+	std::string m_gridLine;
+	std::string m_borderLine;
+	std::string m_friendlyBanner;
 
 	public:
 	Display();
