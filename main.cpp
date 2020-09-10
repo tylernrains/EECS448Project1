9874cb43854d50598a030f46a6f6ff8fc108147e
Hunter Glass <h_glass@ku.edu>
2020-09-10 10:49:30 -0500

    Removed unneccessary comments from main.cpp

--- main.cpp
+++ main.cpp
@@ -3,39 +3,6 @@
 #include "Executive.h"
 
 int main(){
-	/*
-	cout << "Battleship Game\n";
-	/*Board board1;
-	int numHits = 0;
-	*/
-
-  /*
-	Display display;
-	//display.genericFrame();
-	char** enemyBoard = display.test_enemyBoard();
-	char** friendlyBoard = display.test_friendlyBoard();
-
-	//display.enemyBoard(enemyBoard, 1, 3);
-	//display.friendlyBoard(friendlyBoard);
-	display.matchFrame(2, 3, enemyBoard, friendlyBoard);
-	display.hit();
-	display.miss();
-  */
- 	// Board board1;
-	// int numHits;
-	
-	// board1.printBoard();
-	// while(numHits < 3)
-	// {
-	// 	cout << "Enter coordinates to hit: ";
-	// 	char row;
-	// 	int col;
-	// 	cin >> row >> col;
-	// 	board1.updateBoard(row, col, 'c');
-	// 	board1.printBoard();
-	// 	numHits++;
-	// }
-	
 
 	Executive exec;
 	exec.run();
