00fd64d92c1eb8446c4b19829c9bfb25d246d7f9
Shujing Wu <s777w427@ku.edu>
2020-09-12 16:08:36 -0500

    add documentation to Executive class

--- Executive.h
+++ Executive.h
@@ -11,13 +11,47 @@ class Executive
 {
 public:
 
-	Executive(){};
-	~Executive(){};
+	Executive(){};		//Constructor 
+	~Executive(){};		//Destructor
+	
+	/**
+ 	* @pre:	None
+ 	* @post: function to hide player info when switch player1 to 2 or player2 to 1
+ 	* @param: print 50 blank line when need to switch player
+ 	**/
 	void WaitEnter();
+	
+	
+	/**
+ 	* @post: run the program
+	* @param: play the Battleship game, the prog gets ship info from players. 
+	* 		  player will fire the enemy ship until there is a winner
+ 	**/
 	void run();
+	
+	/**
+ 	* @pre: input the correct char type
+ 	* @post: convert letters to corresponding numbers
+	* @param: get the letter from the player, then use the formula to convert letter to numbers 
+	* @return: return ((toupper(c) - 65))
+ 	**/
 	int charToInt(char c);
-	bool validColumn(char c); // Function for verifying user input
-  int numShipCoords(int shipNum);
+	
+	/**
+ 	* @pre: input the correct char type
+ 	* @post: verifying user input if it's within the range of A-I
+	* @param: get the letter from the player, then check the range
+	* @return: return true when the letter is within the range of A-I
+ 	**/
+	bool validColumn(char c);
+	
+	/**
+ 	* @pre: input the correct int type
+ 	* @post: add the number of grids occupied by all ships
+	* @param: get the ship number from the player, then calculate the number of grids occupied by the ship
+	* @return: return n, the number of grids occupied by all ships
+ 	**/
+  	int numShipCoords(int shipNum);
 
 };
 #endif // EXECUTIVE_H
