b207c3731a7bc7a468711673b02a39e2952e34e1
Hunter Glass <h_glass@ku.edu>
2020-09-12 14:27:58 -0500

    Added documentation to board class

--- board.h
+++ board.h
@@ -7,11 +7,33 @@ using namespace std;
 class Board 
 {
 	public:
+		/**
+ 		* @pre: initializes a blank 9x9 board with '-' values
+ 		**/
 		Board();   	    // Constructor
 		~Board();  	    // Destructor
-		void printBoard();  // Print board
+
+		/**
+ 		* @pre: function to print a board, used to test
+ 		* @post: prints the 9x9 board specific to the class
+		**/
+		void printBoard();
+
+		/**
+ 		* @pre: function to place a hit on the board
+		* @post: updates the board with whatever value is passed in
+		* @param: int row: row to update, int col: column to update, char c: character to update row/col with
+ 		**/
 		void updateBoard(int row, int col, char c); // Function to update board
+
+		/**
+ 		* @pre: function to return the value at some coordinates
+		* @post: returns the value at a certain coordinate on the board
+		* @param: int row: row to get value at, int col: column to get value at
+		* @return: returns the character at the specified value
+ 		**/
 		char getValue(int row, int col); // gets value at coordinates
+
 		char m_board[9][9];  
 
 	private:
