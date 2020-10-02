302024b185c7800aa87f17bc7358dc5d5d53af7b
Joshua Berkley <j708b537@ku.edu>
2020-10-02 14:32:13 -0500

    fixed bad names

--- Executive.cpp
+++ Executive.cpp
@@ -584,11 +584,11 @@ void Executive::run_PvAi() {
 	}
 }
 
-void Executive::firetorpedo(int turns, int firepostion, bool iscol)
+void Executive::firetorpedo(int turns, int firepostion, bool isrow)
 {
 	if (turns % 2 == 1)// see if it's player1 or player2
 	{
-		if (iscol == true)// see if it's a row or col to fire from
+		if (isrow == true)// see if it's a row or col to fire from
 		{
 			for (int i = 0; i < 9; i++)
 			{
@@ -641,7 +641,7 @@ void Executive::firetorpedo(int turns, int firepostion, bool iscol)
 	}
 	else
 	{
-		if (iscol == true) // see if it's a row or col to fire from
+		if (isrow == true) // see if it's a row or col to fire from
 		{
 			for (int i = 0; i < 9; i++)
 			{
